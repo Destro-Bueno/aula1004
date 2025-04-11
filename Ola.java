a88c0cf2aa2e257bff0199e753565dfa0ee488c7
Destro-Bueno <bruno.o.bueno@aluno.senai.br>
2025-04-11 08:28:39 -0300

    Add UI

--- Ola.java
+++ Ola.java
@@ -1,5 +1,7 @@
+import javax.swing.JOptionPane;
 public class Ola{
     public static void main(String[] args){
-        System.out.println("Boa tarde á todos!");
+        //System.out.println("Boa tarde á todos!");
+        JOptionPane.showMessageDialog(null, "Tlgd q é o Destro");
     }
 }
